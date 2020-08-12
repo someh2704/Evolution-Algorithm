5e18c85764477a5408391358f1bc707fc81dd208
someh2704 <someh2705@gmail.com>
2020-08-12 15:37:57 +0900

    Using json file for Status class

--- Status.py
+++ Status.py
@@ -1,28 +1,39 @@
+import json
+import uuid
+
 class Status:
-    def __init__(self):
-        # 전투와 관련된 변수
-        self.health = 0
-        self.max_health = 0
-        self.health_regen = 0
-        self.armor = 0
+    def __init__(self, file):
+        with open(file, 'r') as f:
+            self.unit_info = json.load(f)
         
-        self.damage = 0
-        self.range = 0
-        self.attack_delay = 0
-        self.delay_count = 0
-        self.can_attack = True
+        self.tribe = self.unit_info["Name"]["tribe"]
+        self.name = self.unit_info["Name"]["Name"]
+        self.uuid = str(uuid.uuid4())
         
-        self.stamina = 0
+        self.health = self.unit_info["Stats"]["Health"]["Health"]
+        self.max_health = self.unit_info["Stats"]["Health"]["MaxHealth"]
+        self.armor = self.unit_info["Stats"]["Health"]["Armor"]
+        self.health_regen = self.unit_info["Stats"]["Health"]["HealthRegen"]
         
-        self.size = 0
-        self.speed = 0
+        self.damage = self.unit_info["Stats"]["Attack"]["Damage"]
+        self.range = self.unit_info["Stats"]["Attack"]["Range"]
+        self.sight = self.unit_info["Stats"]["Attack"]["Sight"]
         
-        self.damage = 0
-        self.range = 0
-        self.delay = 0
+        self.walk_speed = self.unit_info["Stats"]["Move"]["WalkSpeed"]
+        self.run_speed = self.unit_info["Stats"]["Move"]["RunSpeed"]
+        self.jump_range = self.unit_info["Stats"]["Move"]["JumpRange"]
         
-        self.age = 0
+        self.age = self.unit_info["Stats"]["Age"]
+        self.stamina = self.unit_info["Stats"]["Stamina"]
         
+        self.attack_delay = self.unit_info["Delay"]["AttackDelay"]
+        self.search_delay = self.unit_info["Delay"]["SearchDelay"]
         
-    
-    
+        self.attack_flag = self.unit_info["Flag"]["AttackFlag"]
+        self.search_flag = self.unit_info["Flag"]["SearchFlag"]
+        
+        del self.unit_info
+        
+if __name__ == "__main__":
+    s = Status("UnitInfo.json")
+    print(s)
